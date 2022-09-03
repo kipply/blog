ad78216d672128cc09850e03edded22f3c9ec8fe
kipply <kipply@kipplts-MBP.lan>
2022-09-03 13:39:49 -0400

    jul aug

--- src/Landing.js
+++ src/Landing.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Row, Col } from 'react-bootstrap';
 
-import meImg from './img/me.jpg';
-import meImg2 from './img/me2.jpg';
-import meImg3 from './img/me3.jpg';
-import meImg4 from './img/me4.jpg';
-import meImg5 from './img/me5.jpg';
+import img1 from './img/1.jpg';
+import img2 from './img/2.jpg';
+import img3 from './img/3.jpg';
+import img4 from './img/4.jpg';
+import img5 from './img/5.jpg';
+import img6 from './img/6.jpg';
+import img7 from './img/7.jpg';
+import img8 from './img/8.jpg';
+import img9 from './img/9.jpg';
+import img10 from './img/10.jpg';
+import img11 from './img/11.jpg';
 
-const meImages = [meImg, meImg2, meImg3, meImg4, meImg5];
+const meImages = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11];
 
 const Landing = () => (
   <div style={{ display: 'block' }}>
